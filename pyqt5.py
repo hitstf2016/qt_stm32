f464b1afdcc5eb258ebf771f9d128396f70c06b5
stf <hitstf2016@163.com>
2019-11-17 22:42:06 +0800

    change ui

--- pyqt5.py
+++ pyqt5.py
@@ -1,13 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import MainWindow
+from MainWindow import *
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    myWin = MyMainWindow()
+    myWin.show()
+    sys.exit(app.exec())
 
-class MyMainWindow(QMainWindow, MainWindow):
-        def __init__(self, parent=None):
-            super(MyMainWindow, self).__init__(parent)
-            self.setupUi(self)
-    if __name__ == '__main__':
-        app = QApplication(sys.argv)
-        w = MyMainWindow()
-        w.show()
-        sys.exit(app.exec_())
