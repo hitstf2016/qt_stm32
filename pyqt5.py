b108fd457a25f0b57d9c7abfed73b6535ae0e52d
stf <hitstf2016@163.com>
2019-12-05 23:04:11 +0800

    add scan thread

--- pyqt5.py
+++ pyqt5.py
@@ -1,10 +1,17 @@
 import sys
+import threading
 import serial
 import serial.tools.list_ports
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from MainWindow import *
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
+    class myThread(threading.Thread):
+        def __init__(self,func,threadID,name):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
@@ -12,16 +19,20 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.list_ports()
 
     def connect_Button_clicked(self):
-        serial_usb = serial.Serial(
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            bytesize=serial.EIGHTBITS,
-            stopbits=serial.STOPBITS_ONE,
-            timeout=1000,
-            write_timeout=1000
-        )
-        serial_usb.open()
+         serial_usb = serial.Serial(
+             baudrate=115200,
+             parity=serial.PARITY_NONE,
+             bytesize=serial.EIGHTBITS,
+             stopbits=serial.STOPBITS_ONE,
+             timeout=1000,
+             write_timeout=1000
+         )
+         serial_usb.open()
 
+    def scan_Button_clicked(self,serial_usb,myThread,scan_board):
+        if serial_usb.is_open:
+            thread1 = myThread(scan_board,1,"Thread-1")
+            thread1.start()
 
     def list_ports(self):
         port_list = list(serial.tools.list_ports.comports())
@@ -30,7 +41,11 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         for port_name in port_serial:
             self.comboBox.addItem(port_name)
 
-if __name__=="__main__":
+    def scan_board(self,serial_usb,thread1):
+        serial_usb.write("acc_info")
+        thread1.
+
+if __name__== "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
     myWin.show()
